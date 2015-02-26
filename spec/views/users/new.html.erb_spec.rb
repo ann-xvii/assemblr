e81fd8c9e5dee004314be6bd008472aa217a517e
An.nA <anna.git3@gmail.com>
2015-02-26 10:24:40 -0800

    add users new test, update routes

--- spec/views/users/new.html.erb_spec.rb
+++ spec/views/users/new.html.erb_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "users/new.html.erb", type: :view do
-  
+  it "should have the content 'Sign up'" do
+  	visit '/signup'
+  	expect(page).to have_content('Sign up')
+  end
 end
