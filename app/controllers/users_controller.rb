b0fb6db170375b8efa136196bb38201b0b602433
An.nA <anna.git3@gmail.com>
2015-02-28 05:56:32 -0800

    finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
   # require a logged in user to edit or update
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   # confirm correct user before allowing edit or update
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user, only: :destroy
   
   def index
   	@users = User.paginate(page: params[:page])
@@ -43,6 +44,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
 
   private
 
@@ -65,6 +72,11 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
+  end
+
+  # Confirms an admin user
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 
 end
