38a3989a0efe93e4a598a5939d146756f920bc32
An.nA <anna.git3@gmail.com>
2015-02-28 03:18:25 -0800

    add remember me feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params) 
   	if @user.save
-  		# handle a successful save
+  		log_in @user
       flash[:success] = "Welcome to Assemblr"
   		redirect_to @user
   	else
