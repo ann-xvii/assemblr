d5cea0d7c4bd7904d2eb14090b778c50eb1544da
An.nA <anna.git3@gmail.com>
2015-02-28 00:00:36 -0800

    use SSL and the Puma webserver in production

--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -16,4 +16,49 @@ describe 'Signing up' do
 		click_button "Create my account"
 		expect(User.count).to eq(1)
 	end
+
+	it "does not allow user to sign up with invalid email address" do
+		expect(User.count).to eq(0)
+		visit "/"
+		click_link "Join"
+		expect(page).to have_content("Sign up")
+
+		fill_in "Name", with: "Jabba the Hut"
+		fill_in "Email", with: "jabba@thehutcom"
+		fill_in "Password", with: "spacegangsterz"
+		fill_in "Confirmation", with: "spacegangsterz"
+
+		click_button "Create my account"
+		expect(User.count).to eq(0)
+	end
+
+	it "does not allow user to sign up with password too short" do
+		expect(User.count).to eq(0)
+		visit "/"
+		click_link "Join"
+		expect(page).to have_content("Sign up")
+
+		fill_in "Name", with: "Jabba the Hut"
+		fill_in "Email", with: "jabba@thehut.com"
+		fill_in "Password", with: "space"
+		fill_in "Confirmation", with: "space"
+
+		click_button "Create my account"
+		expect(User.count).to eq(0)
+	end
+
+	it "does not allow user to sign up with invalid password" do
+		expect(User.count).to eq(0)
+		visit "/"
+		click_link "Join"
+		expect(page).to have_content("Sign up")
+
+		fill_in "Name", with: "Jabba the Hut"
+		fill_in "Email", with: "jabba@thehut.com"
+		fill_in "Password", with: "space_star_ordering"
+		fill_in "Confirmation", with: "space"
+
+		click_button "Create my account"
+		expect(User.count).to eq(0)
+	end
 end
