ac43293ff13f1551b8a7c5c028b61b9a1028e32e
An.nA <anna.git3@gmail.com>
2015-03-01 02:00:42 -0800

    add password reset resources

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	attr_accessor :remember_token, :activation_token
+	attr_accessor :remember_token, :activation_token, :reset_token
 	
   before_save :downcase_email
   before_create :create_activation_digest
@@ -58,17 +58,34 @@ class User < ActiveRecord::Base
       UserMailer.account_activation(self).deliver_now
     end
 
+    # Sets the password reset attributes
+    def create_reset_digest
+      self.reset_token = User.new_token
+      update_attribute(:reset_digest, User.digest(reset_token))
+      update_attribute(:reset_sent_at, Time.zone.now)
+    end
+
+    # Sends password reset email
+    def send_password_reset_email
+      UserMailer.password_reset(self).deliver_now
+    end
+
+    # Returns true if a password reset has expired
+    def password_reset_expired?
+      reset_sent_at < 2.hours.ago
+    end
+
     private
 
-    # converts email to all lowercase
-    def downcase_email
-      self.email = email.downcase
-    end
+          # converts email to all lowercase
+          def downcase_email
+            self.email = email.downcase
+          end
 
-    # creates and assigns the activation token digest
-    def create_activation_digest
-      # the main purpose fo the before_create callback is to assign the token and corresponding digest
-      self.activation_token = User.new_token
-      self.activation_digest = User.digest(activation_token)
-    end
+          # creates and assigns the activation token digest
+          def create_activation_digest
+            # the main purpose fo the before_create callback is to assign the token and corresponding digest
+            self.activation_token = User.new_token
+            self.activation_digest = User.digest(activation_token)
+          end
 end
