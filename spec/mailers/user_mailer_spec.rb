7f85f3766e1543159c3ea18d2c0d90151230f377
An.nA <anna.git3@gmail.com>
2015-02-28 23:06:45 -0800

    add mailer tests

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -2,12 +2,16 @@ require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
   describe "account_activation" do
-    let(:mail) { UserMailer.account_activation }
+    
+    user = FactoryGirl.build(:user, name: "Roger Moore", email: "to@example.org", password: "password")
+    let(:mail) { UserMailer.account_activation(user) }
+    user.activation_token = User.new_token
+
 
     it "renders the headers" do
       expect(mail.subject).to eq("Account activation")
       expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.from).to eq(["noreply@example.com"])
     end
 
     it "renders the body" do
@@ -21,7 +25,7 @@ RSpec.describe UserMailer, type: :mailer do
     it "renders the headers" do
       expect(mail.subject).to eq("Password reset")
       expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.from).to eq(["noreply@example.com"])
     end
 
     it "renders the body" do
