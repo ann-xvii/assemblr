49743b4a3b4e0ebf729677a19c60753b19041ba2
An.nA <anna.git3@gmail.com>
2015-03-02 20:23:51 -0800

    add relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
 	
 
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
 
   before_save :downcase_email
   before_create :create_activation_digest
