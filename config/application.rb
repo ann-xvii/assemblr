720fac0b0f69649ae8828f82c6084ef44c7dd9d8
An.nA <anna.git3@gmail.com>
2015-03-07 02:19:39 -0800

    add favico, add fonts, precompile, update style and css

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module GeneralAssemblr
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.action_view.embed_authenticity_token_in_remote_forms = true
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
