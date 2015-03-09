18a92d12797bcefe7fe0f6b3d84e11e32842744d
An.nA <anna.git3@gmail.com>
2015-03-08 17:22:43 -0700

    add image assets, add profiles controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
+  get '/profiles' => 'profiles#index'
 
   # USER CONTROLLER ROUTES
   get 'signup' => 'users#new'
