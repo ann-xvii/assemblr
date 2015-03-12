942617040f59047c0f25ebdb549a1f4b2f1e2d30
An.nA <anna.git3@gmail.com>
2015-03-12 09:00:07 -0700

    fix minor bugs

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
-  get '/profiles' => 'profiles#index'
+  get '/profiles' => 'profiles#index', as: :profiles
 
   # USER CONTROLLER ROUTES
   get 'signup' => 'users#new'
